32ee43e19340b389fb9f1cde71ef96ea01f85364
Amy Evans <amykarline@yahoo.com>
2019-10-08 13:29:01 -0700

    added Dubai

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var shopHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm',
 
 var ulSeattle = document.getElementById('seattle');
 
-var cookieshopSeattle = {
+var cookieshopDubai = {
   minimumCustomers: 23,
   maximumCustomers: 65,
   averageCookiesPerCustomer: 6.3,
@@ -46,9 +46,9 @@ var cookieshopSeattle = {
   }
 };
 
-cookieshopSeattle.generateCustomersPerHour();
-cookieshopSeattle.generateCookiesPerHour();
-cookieshopSeattle.render();
+cookieshopDubai.generateCustomersPerHour();
+cookieshopDubai.generateCookiesPerHour();
+cookieshopDubai.render();
 
 var ulTokyo = document.getElementById('tokyo');
 
@@ -96,6 +96,53 @@ var cookieshopTokyo = {
 cookieshopTokyo.generateCustomersPerHour();
 cookieshopTokyo.generateCookiesPerHour();
 cookieshopTokyo.render();
+
+var ulDubai = document.getElementById('dubai');
+
+var cookieshopDubai = {
+  minimumCustomers: 11,
+  maximumCustomers: 38,
+  averageCookiesPerCustomer: 3.7,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookiesEachDay: 0,
+
+
+  generateCustomersPerHour: function(){
+    for (var i = 0; i < shopHours.length; i++){
+    // generate a random number
+      var customers = randomNumber(this. minimumCustomers, this.maximumCustomers);
+      // this stores it in an array
+      this.customersPerHour.push(customers);
+    }
+  },
+  generateCookiesPerHour: function() {
+    this.generateCustomersPerHour();
+    for (var i = 0; i < shopHours.length; i++) {
+      var cookiesOneHour = Math.ceil(this.customersPerHour[i] * this.averageCookiesPerCustomer);
+      this.cookiesPerHour.push(cookiesOneHour);
+      this.totalCookiesEachDay += cookiesOneHour;
+    }
+  },
+  render: function() {
+    for (var i = 0; i < shopHours.length; i++){
+      // create an element
+      var liEL = document.createElement('li');
+      // add content
+      liEL.textContent = `${shopHours[i]} Cookies: ${this.cookiesPerHour[i]}`;
+      // append to the DOM; generateCookiesPerHour
+      ulDubai.appendChild(liEL);
+    }
+    // eslint-disable-next-line no-redeclare
+    var liEL = document.createElement('li');
+    liEL.textContent = `Daily Total: ${this.totalCookiesEachDay}`;
+    ulDubai.appendChild(liEL);
+  }
+};
+
+cookieshopDubai.generateCustomersPerHour();
+cookieshopDubai.generateCookiesPerHour();
+cookieshopDubai.render();
 
 function randomNumber (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
